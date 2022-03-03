3e16d304d70efd9d1f4416ce06a5b20bd3ba9309
kohta1208 <kohtafms@gmail.com>
2022-03-03 21:55:08 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueQriously from 'vue-qriously'
-
 Vue.config.productionTip = false
+// compatを追加する
+import { initializeApp } from "firebase/app";
+// TODO: Replace the following with your app's Firebase project configuration
+import { firebaseConfig } from './firebase-config.js';
+const firebaseApp = initializeApp(firebaseConfig);
 
+export { firebaseApp };
 Vue.use(VueQriously)
 
 new Vue({
